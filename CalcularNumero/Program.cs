5c1be0f47df81d0fbda79066504c2e375c66a433
William <fpmw.50@gmail.com>
2022-10-12 19:21:25 -0500

    Se realizaron mas operaciones

--- CalcularNumero/Program.cs
+++ CalcularNumero/Program.cs
@@ -22,6 +22,10 @@ namespace CalcularNumero
             Console.WriteLine("RAIZ CUADRADA : " + Math.Sqrt(NUM1));
             Console.WriteLine("SENO : " + Math.Sin(NUM1 * Math.PI / 180));
             Console.WriteLine("COSENO : " + Math.Cos(NUM1 * Math.PI / 180));
+            Console.WriteLine("NÚMERO MÁXIMO : " + Math.Max(NUM1, 50));
+            Console.WriteLine("NÚMERO MÍNIMO : " + Math.Min(NUM1, 50));
+            Console.WriteLine("PARTE ENTERA : " + Math.Truncate(18.78));
+            Console.WriteLine("REDONDEO : " + Math.Round(18.78));
             Console.Write("Pulse una Tecla:");
             Console.ReadLine();
         }
